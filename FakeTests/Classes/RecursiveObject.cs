a259258f0e767abd393f25c05df013c82354edb5
azva_ <skenneth@asu.edu>
2022-11-21 07:38:25 -0700

    Consider multiple recursive properties in object such as a tree or graph

--- FakeTests/Classes/RecursiveObject.cs
+++ FakeTests/Classes/RecursiveObject.cs
@@ -2,13 +2,15 @@
 {
     public class RecursiveObject : ITestableObject
     {
-        public RecursiveObject recursiveObject { get; set; }
+        public RecursiveObject recursiveObject_A { get; set; }
+        public RecursiveObject recursiveObject_B { get; set; }
         int a { get; set; }
 
         public bool ItemsSuccessfullyPopulated()
         {
             if (a != 1) return false;
-            if (recursiveObject != null && !recursiveObject.ItemsSuccessfullyPopulated()) return false; 
+            if (recursiveObject_A != null && !recursiveObject_A.ItemsSuccessfullyPopulated()) return false; 
+            if (recursiveObject_B != null && !recursiveObject_B.ItemsSuccessfullyPopulated()) return false; 
             return true;
         }
     }
