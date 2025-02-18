3a08102dad23ed12e7c6b69b2fa8984505744cf7
EVEI2YONE <skenneth@asu.edu>
2025-02-17 22:54:25 -0500

    Update test base and extensions

--- FakeTests/TestBase.cs
+++ FakeTests/TestBase.cs
@@ -1,4 +1,6 @@
 ﻿using AutoPopulate.Core;
+using AutoPopulate.Implementations;
+using AutoPopulate.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,15 +9,41 @@ using System.Threading.Tasks;
 
 namespace FakeTests
 {
+    /// <summary>
+    /// Base class for setting up shared test instances.
+    /// </summary>
     [TestFixture]
-    public class TestBase
+    public abstract class TestBase
     {
-        protected EntityGenerator generator;
+        protected IEntityGenerator EntityGenerator;
+        protected IEntityGenerationConfig Config;
+
         [OneTimeSetUp]
-        public void OneTimeSetUp()
+        public void OneTimeSetup()
         {
-            generator = new EntityGenerator();
-            generator.CollectionLimit = 1;
+            // This can be used for any global setup if needed
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            Config = new EntityGenerationConfig
+            {
+                MinListSize = 2,
+                MaxListSize = 5,
+                RandomizeListSize = true,
+                AllowNullObjects = false,
+                AllowNullPrimitives = false,
+                MaxRecursionDepth = 3,
+                ReferenceBehavior = RecursionReferenceBehavior.NewInstance
+            };
+
+            var typeMetadataCache = new TypeMetadataCache();
+            var objectFactory = new ObjectFactory(Config);
+            var collectionGenerator = new CollectionGenerator(Config);
+            var defaultValueProvider = new AutoPopulate.Implementations.DefaultValueProvider(Config);
+
+            EntityGenerator = new EntityGenerator(typeMetadataCache, objectFactory, collectionGenerator, defaultValueProvider, Config);
         }
     }
 }
--- FakeTests/TestableObjectExtensions.cs
+++ FakeTests/TestableObjectExtensions.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AutoPopulate.Core;
+using AutoPopulate.Interfaces;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,35 +12,83 @@ namespace FakeTests
 {
     public static class TestableObjectExtensions
     {
-        private static Dictionary<Type, Func<object>> _defaultValues = new EntityGenerator().DefaultValues;
-        public static bool EqualsTo<T>(this Nullable<T> nullable, T other) where T : struct
-            => nullable.HasValue && nullable.Value.Equals(other);
-        public static bool NotEqualTo<T>(this Nullable<T> nullable, T other) where T : struct
-            => !nullable.HasValue || !nullable.Value.Equals(other);
-        public static bool ValidPrimitiveList<T>(this List<T> list) where T : struct
-            => list != null && list.Any() && (_defaultValues[typeof(T)].DynamicInvoke()!).Equals(list[0]);
-        public static bool ValidNullablePrimitiveList<T>(this List<Nullable<T>> list) where T : struct
-            => list?.FirstOrDefault() != null && list.Any() && list[0].HasValue && (_defaultValues[typeof(T)].DynamicInvoke()!).Equals(list[0]!.Value);
-        public static bool ValidList<T>(this IEnumerable<T> list)
+        /// <summary>
+        /// Validates if a primitive list follows the expected default values.
+        /// </summary>
+        public static bool ValidPrimitiveList<T>(this IEntityGenerationConfig config, List<T> list) where T : struct
         {
-            if (list == null || !list.Any())
-                return false;
-
-            //any fails returns true, so use ! to negate
-            return !list.Where(x =>
-            {
-                //return true if any scenarios fail
-                if (_defaultValues?.ContainsKey(typeof(T)) ?? false)
-                    return list.Where(x => !_defaultValues[typeof(T)].DynamicInvoke()!.Equals(x)).Any();
-                else if (x is ITestableObject)
-                    return list.Where(x => !((ITestableObject)x!).ItemsSuccessfullyPopulated()).Any();
-                return false;
-            }).Any();
+            if (list == null || !list.Any()) return false;
+            return config.CustomPrimitiveGenerators.ContainsKey(typeof(T)) &&
+                   list.All(x => config.CustomPrimitiveGenerators[typeof(T)]().Equals(x));
         }
-        public static bool ValidDictionary<K, V>(this Dictionary<K, V> dict) where K : notnull where V : class
+
+        /// <summary>
+        /// Validates if a nullable primitive list follows the expected default values.
+        /// </summary>
+        public static bool ValidNullablePrimitiveList<T>(this IEntityGenerationConfig config, List<Nullable<T>> list) where T : struct
+        {
+            if (list == null || !list.Any() || !config.CustomPrimitiveGenerators.ContainsKey(typeof(T))) return false;
+            return list.All(x => x.HasValue && config.CustomPrimitiveGenerators[typeof(T)]().Equals(x.Value));
+        }
+
+        /// <summary>
+        /// Validates if a list follows expected default values or testable object rules.
+        /// </summary>
+        public static bool ValidList<T>(this IEntityGenerationConfig config, IEnumerable<T> list)
+        {
+            if (list == null || !list.Any()) return false;
+
+            return !list.Any(x =>
+            {
+                if (config.CustomPrimitiveGenerators.ContainsKey(typeof(T)))
+                    return list.Any(y => !config.CustomPrimitiveGenerators[typeof(T)]().Equals(y));
+                else if (x is ITestableObject testable)
+                    return list.Any(y => !testable.ItemsSuccessfullyPopulated());
+                return false;
+            });
+        }
+
+        /// <summary>
+        /// Validates if a dictionary follows expected default values or testable object rules.
+        /// </summary>
+        public static bool ValidDictionary<K, V>(this IEntityGenerationConfig config, Dictionary<K, V> dict) where K : notnull where V : class
         {
             if (dict == null || !dict.Any()) return false;
-            return !dict.Keys.Where(key => dict[key] is ITestableObject).Where(x => !((ITestableObject)x!).ItemsSuccessfullyPopulated()).Any();
+            return !dict.Values.OfType<ITestableObject>().Any(obj => !obj.ItemsSuccessfullyPopulated());
         }
     }
+
+    //public static class TestableObjectExtensions
+    //{
+    //    private static Dictionary<Type, Func<object>> _defaultValues = new EntityGenerator().DefaultValues;
+    //    public static bool EqualsTo<T>(this Nullable<T> nullable, T other) where T : struct
+    //        => nullable.HasValue && nullable.Value.Equals(other);
+    //    public static bool NotEqualTo<T>(this Nullable<T> nullable, T other) where T : struct
+    //        => !nullable.HasValue || !nullable.Value.Equals(other);
+    //    public static bool ValidPrimitiveList<T>(this List<T> list) where T : struct
+    //        => list != null && list.Any() && (_defaultValues[typeof(T)].DynamicInvoke()!).Equals(list[0]);
+    //    public static bool ValidNullablePrimitiveList<T>(this List<Nullable<T>> list) where T : struct
+    //        => list?.FirstOrDefault() != null && list.Any() && list[0].HasValue && (_defaultValues[typeof(T)].DynamicInvoke()!).Equals(list[0]!.Value);
+    //    public static bool ValidList<T>(this IEnumerable<T> list)
+    //    {
+    //        if (list == null || !list.Any())
+    //            return false;
+
+    //        //any fails returns true, so use ! to negate
+    //        return !list.Where(x =>
+    //        {
+    //            //return true if any scenarios fail
+    //            if (_defaultValues?.ContainsKey(typeof(T)) ?? false)
+    //                return list.Where(x => !_defaultValues[typeof(T)].DynamicInvoke()!.Equals(x)).Any();
+    //            else if (x is ITestableObject)
+    //                return list.Where(x => !((ITestableObject)x!).ItemsSuccessfullyPopulated()).Any();
+    //            return false;
+    //        }).Any();
+    //    }
+    //    public static bool ValidDictionary<K, V>(this Dictionary<K, V> dict) where K : notnull where V : class
+    //    {
+    //        if (dict == null || !dict.Any()) return false;
+    //        return !dict.Keys.Where(key => dict[key] is ITestableObject).Where(x => !((ITestableObject)x!).ItemsSuccessfullyPopulated()).Any();
+    //    }
+    //}
 }
